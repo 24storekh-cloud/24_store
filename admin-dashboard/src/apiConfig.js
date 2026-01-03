7c497c31fa4ce020f938e96abb63aff263a04a72
MarketxTrader <noyvathana8900@gmail.com>
2026-01-03 18:47:30 +0700

    update server port

--- admin-dashboard/src/apiConfig.js
+++ admin-dashboard/src/apiConfig.js
@@ -1,6 +1,6 @@
-// admin-dashboard/src/apiConfig.js  និង client/src/apiConfig.js
+// admin-dashboard/src/apiConfig.js និង client/src/apiConfig.js
 const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://24store-api.onrender.com' // <-- ផាស Link របស់បងនៅទីនេះ
+  ? 'https://two4-store.onrender.com'  // <--- នេះជា Link ពិតរបស់បង
   : 'http://localhost:5000';
 
 export default API_URL;
